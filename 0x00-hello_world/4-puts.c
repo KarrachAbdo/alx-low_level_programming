69399c22abf922a2780df7c9b3c788c9309ce743
KARRACH <karrach.abdellah.dev@gmail.com>
2023-03-16 07:20:52 +0000

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,7 @@
  * main - A program that print 'Programming is like building a
  * multilingual puzzle' using puts
  *
- * return :0 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {puts("\"Programming is like building a multilingual puzzle");
