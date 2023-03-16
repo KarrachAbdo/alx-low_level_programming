ccdac172b6ea1906da628586b52857b538b66d34
KARRACH <karrach.abdellah.dev@gmail.com>
2023-03-16 07:08:55 +0000

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 /**
- * main - A program that print 'Programming is like building a multilingual puzzle' using puts
+ * main - A program that print 'Programming is like building a
+ * multilingual puzzle' using puts
  * return :0 (Success)
  */
-int main(void) {   
- puts("\"Programming is like building a multilingual puzzle");
+int main(void)
+{   
+puts("\"Programming is like building a multilingual puzzle");
 return(0);
- }
+}
