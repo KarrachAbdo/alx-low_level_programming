9bbdaca28a43455324aaf48596b04eb573f3cf0a
KARRACH <karrach.abdellah.dev@gmail.com>
2023-03-16 06:49:44 +0000

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+/*programme that print 'Programming is like building a multilingual puzzle' using puts */
+
+int main() {   
+    puts("Programming is like building a multilingual puzzle");
+return 0;
+ }
