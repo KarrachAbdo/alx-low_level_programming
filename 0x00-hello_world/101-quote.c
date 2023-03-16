d121d43c13c0b69c687a27049c4931eaa44e78ce
KARRACH <karrach.abdellah.dev@gmail.com>
2023-03-16 08:59:19 +0000

    101

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - program that prints exactly and that piece of art is useful - Dora Korpar, 2015-10-19
+ * Return: Always 1 (Success)
+ * */
+int main(void)
+{fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
+}
