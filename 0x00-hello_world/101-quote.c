06a8f349cc13eedb40f06ff0c0031adaeb6bb749
KARRACH <karrach.abdellah.dev@gmail.com>
2023-03-16 09:05:51 +0000

    101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,6 +5,6 @@
  * Return: Always 1 (Success)
  * */
 int main(void)
-{fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+{fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 return (1);
 }
