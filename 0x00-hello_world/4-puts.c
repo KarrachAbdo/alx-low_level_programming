63960e35fed09306d9d167875612ee63b71dc02d
KARRACH <karrach.abdellah.dev@gmail.com>
2023-03-16 07:16:17 +0000

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,7 +5,6 @@
  * return :0 (Success)
  */
 int main(void)
-{   
-puts("\"Programming is like building a multilingual puzzle");
+{puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
