aed88b7d57f1e53346133520d9dca77bba46b771
KARRACH <karrach.abdellah.dev@gmail.com>
2023-03-17 12:06:05 +0000

    6

---
+++ 0x01-variables_if_else_while/6-print_numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - A program
+ * Return: Always 0 (Success)
+ **/
+int main(void)
+{
+int = 0;
+for (i = 0 ; i < 10 ; i++)
+putchar(i + 48);
+putchar('\n');
+return (0);
+}
