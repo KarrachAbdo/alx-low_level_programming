87fcfee60fb61429700faf9a7793d25578249eac
KARRACH <karrach.abdellah.dev@gmail.com>
2023-03-17 14:02:09 +0000

    9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - A program
+ * Return: Always 0 (Success)
+ **/
+int main(void)
+{
+int  i;
+for (i = 0 ; i < 10 ; i++)
+{
+putchar(i + 48);
+if (i != 9)
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
