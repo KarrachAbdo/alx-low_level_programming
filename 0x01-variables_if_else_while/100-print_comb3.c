d91938e6d9b0ee0b4925888c91e98bd5ef7965ab
KARRACH <karrach.abdellah.dev@gmail.com>
2023-03-17 15:18:29 +0000

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,24 +11,12 @@ for (i = 0; i < 9; i++)
 {
 for (j = i + 1; j <= 9; j++)
 {
-if (i == 0)
-{
-putchar('0' + i);
-putchar('0' + j);
+putchar(i + 48);
+putchar(j + 48);
 if (i != 8)
 {
 putchar(',');
 putchar(' ');
-}}
-else
-{
-putchar('0' + i);
-putchar('0' + j);
-if (i != 8)
-{
-	putchar(',');
-	putchar(' ');
-}
 }
 }
 }
