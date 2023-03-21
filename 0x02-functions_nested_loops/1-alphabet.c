2f59f0e7e13550a575d4b7f77eb25ed67fb16515
KARRACH <karrach.abdellah.dev@gmail.com>
2023-03-21 12:05:41 +0000

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+#include "main.h"
+/**
+ * print_alphabet(void) - 
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+void print_alphabet(void)
+{
+char i;
+for(i = 'a'; i <= 'z'; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
+}
