d25c4c02864f50ba7f94e462700dae91e1bceac0
KARRACH <karrach.abdellah.dev@gmail.com>
2023-03-21 20:54:05 +0000

    103

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -6,13 +6,13 @@
  */
 int main(void)
 {
-long n1 = 1, n2 = 2;
+long n1 = 1, n2 = 2, next;
 long  sum = 0;
 while (n2 <= 4000000)
 {
 if (n2 % 2 == 0)
 sum += n2;
-long next = n1 + n2;
+next = n1 + n2;
 n1 = n2;
 n2 = next;
 }
