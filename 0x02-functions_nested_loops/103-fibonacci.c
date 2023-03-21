ca5c361fbd7cb4f6d6eba359dac684cc107d219a
KARRACH <karrach.abdellah.dev@gmail.com>
2023-03-21 20:52:45 +0000

    103

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+long n1 = 1, n2 = 2;
+long  sum = 0;
+while (n2 <= 4000000)
+{
+if (n2 % 2 == 0)
+sum += n2;
+long next = n1 + n2;
+n1 = n2;
+n2 = next;
+}
+printf("%ld\n", sum);
+return (0);
+}
