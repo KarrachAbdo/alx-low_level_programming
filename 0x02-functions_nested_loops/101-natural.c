16627983ee31302c54c1e7f216e7157235791e77
KARRACH <karrach.abdellah.dev@gmail.com>
2023-03-21 20:30:46 +0000

    101

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -9,11 +9,9 @@ int main(void)
 {
 int sum = 0;
 int i;
-for (i = 0; i <= 1024; i++)
+for (i = 0; i < 1024; i++)
 {
-if ((i % 3) == 0)
-sum = sum + i;
-if ((i % 5) == 0)
+if ((i % 3) == 0 || (i % 5) == 0)
 sum = sum + i;
 }
 printf("%d", sum);
