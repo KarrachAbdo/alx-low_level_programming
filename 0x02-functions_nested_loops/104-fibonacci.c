92370dec36a61704f04fb42d894b9af0b3d7edde
KARRACH <karrach.abdellah.dev@gmail.com>
2023-03-21 21:14:54 +0000

    104

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - check the code.
+ * Return: Always 0.
+ **/
+void fibo(void)
+{
+int a = 1, b = 2, c;
+printf("%d, %d, ", a, b);
+for (int i = 2; i < 98; i++)
+{
+c = a + b;
+printf("%d, ", c);
+a = b;
+b = c;
+}
+printf("\n");
+}
+int main(void)
+{
+fibo();
+return (0);
+}
