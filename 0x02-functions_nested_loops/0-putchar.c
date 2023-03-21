b65067dcbf106b1cf82f7a1110981993ae8ab046
KARRACH <karrach.abdellah.dev@gmail.com>
2023-03-21 11:14:18 +0000

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - program
+ * Return: Always 0 (Success)
+ **/
+int main(void)
+{
+	putchar(95);
+	putchar(112);
+	putchar(117);
+	putchar(116);
+	putchar(99);
+	putchar(104);
+	putchar(97);
+	putchar(114);
+	putchar('\n');
+	return (0);
+}
