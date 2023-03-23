4a67b3fdfd84d6a6bf3d460dca7593cfdc9dbb9f
KARRACH <karrach.abdellah.dev@gmail.com>
2023-03-23 10:37:56 +0000

    most numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+*print_most_numbers - print 01356789
+*return: void
+**/
+
+void print_most_numbers(void)
+{
+char c;
+
+for (c = '0'; c <= '9'; c++)
+{
+if (c != '2' || c != '4')
+_putchar(c);
+}
+_putchar('\n');
+}
+
