9cfe4a183cd458824780d54d0cb0abf0ac2ab3d7
KARRACH <karrach.abdellah.dev@gmail.com>
2023-03-23 10:28:56 +0000

    numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* print_numbers - print numbers
+* Return: 0
+**/
+void print_numbers(void)
+{
+char i;
+for (i = '0'; i <= '9'; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
+
+return (0);
+}
+
