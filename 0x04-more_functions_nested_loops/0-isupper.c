ccb478ba9e40a5705a840c68ac3c9b41133d2de0
KARRACH <karrach.abdellah.dev@gmail.com>
2023-03-23 09:52:13 +0000

    0-main

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+#include "main.h"
+/**
+ *_isupper - function that checks for lowercase.
+ *@c : the caractere to be cheched
+ * Return: On success 1.
+ * On otherwise, 0.
+ **/
+int _isupper(int c)
+{
+if (c >= 64 && c <= 90)
+return (1);
+return (0);
+}
