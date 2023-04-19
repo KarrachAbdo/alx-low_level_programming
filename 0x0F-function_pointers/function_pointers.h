33951733a6872ba8b2ad733d42867fdeea9c9776
KARRACH <karrach.abdellah.dev@gmail.com>
2023-04-19 12:13:55 +0000

    print array

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,4 +1,6 @@
 #ifndef FUNCTION_POINTERTS_H
 #define FUNCTION_POINTERTS_H
+#include <stddef.h>
 void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
 #endif
