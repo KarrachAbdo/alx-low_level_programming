cf55db2d079594c3d6776ba3dcc76eee658b33ea
KARRACH <karrach.abdellah.dev@gmail.com>
2023-04-28 14:07:31 +0000

    fix

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -18,5 +18,6 @@ struct list_s *next;
 list_t *add_node_end(list_t **head, const char *str);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
+void free_list(list_t *head);
 list_t *add_node(list_t **head, const char *str);
 #endif
