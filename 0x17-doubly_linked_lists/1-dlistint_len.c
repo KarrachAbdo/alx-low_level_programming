c18ca8298fa39866a8de43e36b4868a1c52e7d49
KARRACH <karrach.abdellah.dev@gmail.com>
2023-06-15 19:16:52 +0100

    tsk 1

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,19 @@
+/**
+ * dlistint_len - Returns the number of elements in a doubly linked list.
+ * @h: Pointer to the head of the linked list.
+ *
+ * Return: The number of elements in the linked list.
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+const dlistint_t *current = h;
+size_t count = 0;
+
+while (current != NULL)
+{
+count++;
+current = current->next;
+}
+
+return (count);
+}
