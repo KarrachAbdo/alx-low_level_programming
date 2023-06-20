471842b602ea900287fc8ff93a40a0d5fe24a844
KARRACH <karrach.abdellah.dev@gmail.com>
2023-06-20 11:12:41 +0100

    tsk 1

---
+++ 0x18-dynamic_libraries/6-abs.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+#include "main.h"
+/**
+ *_abs - function that print abs value.
+ * @c : the value to be compute
+ *Return: int
+ **/
+int _abs(int c)
+{
+int val_abs;
+if (c < 0)
+{
+val_abs = -1 * c;
+return (val_abs);
+}
+else
+return (c);
+}
